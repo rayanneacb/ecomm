f4a70ba34a570193b731bd214650dba04f26a03a
BRANCO RAYANNE FERNANDA ALVES CASTELO <n851145@gruposantander.com>
2022-11-24 20:21:42 -0300

    Mudando Formato da Data

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,20 +1,14 @@
 var accounts = [];
 export function createUserUseCase(nome, email, senha){
-   function dataAtualFormatada() {
-      var data = new Date(),
-        dia = data.getDate().toString(),
-        diaF = dia.length == 1 ? "0" + dia : dia,
-        mes = (data.getMonth() + 1).toString(),
-        mesF = mes.length == 1 ? "0" + mes : mes,
-        anoF = data.getFullYear();
-      return diaF + "/" + mesF + "/" + anoF;
-    }  
+   const data = new Date();
+   let dataFormatada = data.toISOString().substring(0, 10);
+   console.log(dataFormatada);
     const account = {
       id: accounts.length+1,
       name: nome,
       email: email,
       password: senha,
-      createdDate: dataAtualFormatada(),
+      createdDate: dataFormatada,
    };
    accounts.push(account)
 
