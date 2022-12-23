3519830eae60e7a06b8f13a01bf1e62b36de1b8d
BRANCO RAYANNE FERNANDA ALVES CASTELO <n851145@gruposantander.com>
2022-12-23 10:15:19 -0300

    Documentando API

--- product/src/main.js
+++ product/src/main.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { router } from './routes.js';
+import swaggerUi from "swagger-ui-express";
+import apiDocs from  "./api-docs.json" assert {type: "json"}; ;
+
 const app = express();
 app.use(express.json());
 app.use(router);
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs));
 app.listen(3000, function () {
     console.log('products server is running');
 });
